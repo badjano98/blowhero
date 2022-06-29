23a5238b46722e23056b118b5b2f7d2030966d68
badjano98 <80923406+badjano98@users.noreply.github.com>
2022-06-29 21:43:45 +0300

    more

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 SensorEndpoint = ""
 window.properties = {
-	"last-updated" : 0
+	"last-updated" : 0,
+	"sensors" : null
 }
 
-sensors = ["BOTTOM", "MIDDLE", "TOP", "LEFT", "RIGHT"]
+sensors = [
+	"BOTTOM",
+	"MIDDLE",
+	"TOP",
+	"LEFT",
+	"RIGHT"
+	]
 LastUpdated = 0
 
-window.properties.current_combination = []
+window.properties.current_combination = {}
 window.properties.sensor_status = {}
 
 var gamewindow = document.createElement("h1");
@@ -14,18 +21,18 @@ gamewindow.innerText = "";
 document.getElementsByTagName("body")[0].appendChild(gamewindow)
 
 var gameLoopInterval = null
+var combinationInterval = null
 
 function changeCurrentCombination(){
 
 	window.properties.current_combination = sensors[Math.floor(Math.random() * sensors.length)]
-	console.log(window.properties.current_combination)
+	// console.log(window.properties.current_combination)
 	gamewindow.innerText = window.properties.current_combination
 }
 
 
 function gameLoop(){
 
-	console.log("update")
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
@@ -38,7 +45,7 @@ function gameLoop(){
 	xhttp.send()
 
 
-    console.log(window.properties.sensor_status);
+    // console.log(window.properties.sensor_status);
 
     var last_updated_tmp = LastUpdated
     LastUpdated = window.properties.sensor_status["last-updated"]
@@ -49,8 +56,17 @@ function gameLoop(){
     	return;
     }
 
+    if (window.properties.sensor_status.sensors == null){
+    	console.log("disconnected")
+    	clearInterval(gameLoopInterval)
+    	return;
+    }
 
-
+    if (window.properties.sensor_status.sensors[window.properties.current_combination] == 1){
+    	log.console("Success!!!")
+    	clearInterval(combinationInterval)
+		combinationInterval = setInterval(changeCurrentCombination, 5000);
+    }
 
 }
 
@@ -58,4 +74,4 @@ function gameLoop(){
 
 gameLoopInterval = setInterval(gameLoop, 250);
 
-setInterval(changeCurrentCombination, 5000);
+combinationInterval = setInterval(changeCurrentCombination, 5000);
