6113c73e4b58ec71b2c9bb2ad49818b29571016e
badjano98 <80923406+badjano98@users.noreply.github.com>
2022-06-29 18:23:35 +0300

    more

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+https://raw.githubusercontent.com/badjano98/blowhero/main/main.js
+SensorEndpoint = ""
 
-console.log(1)
-alert("This is from the browser!")
+function gameloop(){
+
+	var sensor_status = null
+	console.log("update")
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    	   // Typical action to be performed when the document is ready:
+    	   sensor_status = xhttp.responseText;
+
+    	}
+	};
+
+	xhttp.open("GET", window.location + "/api/v1/sensor-status", true);
+	xhttp.send()
+
+	while (sensor_status == null){}
+    console.log(sensor_status);
+	console.log(window.location)
+	
+
+}
+
+
+setTimeout(gameLoop, 250);
+
