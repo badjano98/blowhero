e49d64515589e7b9b5e4a057989d973947bad56c
badjano98 <80923406+badjano98@users.noreply.github.com>
2022-06-29 19:22:45 +0300

    more

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 SensorEndpoint = ""
+window.properties = {}
 
 function gameLoop(){
 
@@ -6,18 +7,16 @@ function gameLoop(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
-    	   // Typical action to be performed when the document is ready:
-    	   window.properties = {}
+ 		// Update sensor status
     	   window.properties.sensor_status = xhttp.responseText;
-
     	}
 	};
     console.log(window.properties.sensor_status);
     console.log("Freezing...");
 
 	// while (window.properties.sensor_status == null){
-		xhttp.open("GET", window.location + "api/v1/sensor-status", false);
-		// xhttp.send()
+	xhttp.open("GET", window.location + "api/v1/sensor-status", false);
+	xhttp.send()
 	// }
     console.log("Unfroze...");
 	console.log(window.location)
